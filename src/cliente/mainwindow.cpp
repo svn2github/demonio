82300814ba662672fbd7326e3bb2abe302e8dc3e
paju1986 <paju1986@2930cdd9-4e21-42d3-ab1c-436ff6b9a56f>
2011-07-15 10:32:03 +0000

    git-svn-id: svn://svn.code.sf.net/p/demonio/code@217 2930cdd9-4e21-42d3-ab1c-436ff6b9a56f

--- src/cliente/mainwindow.cpp
+++ src/cliente/mainwindow.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2010 Alberto Pajuelo Montes <paju1986@gmail.com>
+ *  Copyright (C) 2011 Alberto Pajuelo Montes <paju1986@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
